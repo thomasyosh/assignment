d9ccf5f1e640a96b923209733c522fcb3a5a3b3f
thomasyosh <149293032+thomasyosh@users.noreply.github.com>
2023-11-17 07:11:17 +0800

    Main.java の更新

--- Main.java
+++ Main.java
@@ -1,22 +1,35 @@
 import java.util.*;
-
+//觀自在菩薩
 class Main{
     
     public static void main(String[] args) {
+        //行深般若波羅蜜多時
         Scanner scanner = new Scanner(System.in);
+        //照見五蘊皆空
         RandomSeed random = null;
+        //度一切苦厄
         Queue queue = new LinkedQueue();
         
+        //舍利子 色不異空 空不異色
         System.out.print("Input simulation time (mins):");
+
+        //色即是空 空即是色
         int minutes = scanner.nextInt();
+        //受想行識 亦復如是
         int actionQueued = 0;
+        //舍利子 是諸法空相
         int numberOfMembersServed = 0;
+        //不生不滅 不垢不淨 不增不減
         boolean randomInput = false;
         
 
+        //是故空中無色
         if (minutes == 0 ){
+        //無受想行識 無眼耳鼻舌身意 無聲色香味觸法
             randomInput = true;
+        //無眼界 乃至無意識界
             minutes = 10;
+        //無無明 亦無無明盡
             System.out.print("Input seed number: ");
             long seed = scanner.nextLong();
             random = new RandomSeed(seed);
